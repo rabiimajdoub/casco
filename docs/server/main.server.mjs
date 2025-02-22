30785f2a56b80e91e47d301c137ab7fd882a2531
Majdoub Rabii <rabii.majdoub@sofrecom.com>
2025-02-22 14:10:08 +0100

    update build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{c as a}from"./chunk-YLWAPB5J.mjs";import"./chunk-GKMWLNPZ.mjs";import"./chunk-MFMHMOPI.mjs";import"./chunk-3WM34J5J.mjs";export{a as default};
+import{c as a}from"./chunk-VMUOXIMS.mjs";import"./chunk-GKMWLNPZ.mjs";import"./chunk-MFMHMOPI.mjs";import"./chunk-3WM34J5J.mjs";export{a as default};
